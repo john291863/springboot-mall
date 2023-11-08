dbda5a319a2c41e73a702455282b3e8f6914e228
john29186352 <johnnesteacena@gmail.com>
2023-11-08 17:59:57 +0800

    帳號功能: 使用MD5加密密碼。

--- src/main/java/com/john/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/john/springbootmall/service/impl/UserServiceImpl.java
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -24,14 +27,19 @@ public class UserServiceImpl implements UserService {
         //檢查是否重複註冊帳號
 
         User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
-        if(user!=null){
+        if (user != null) {
             log.warn("使用者Email: {}, 已被註冊", userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+        //使用MD5 生成密碼雜湊值
+
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // 成功註冊帳號
-        else {
-            return userDao.register(userRegisterRequest);
-        }
+
+        return userDao.register(userRegisterRequest);
+
     }
 
     @Override
@@ -42,16 +50,20 @@ public class UserServiceImpl implements UserService {
     @Override
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
-
-        if (user==null){
-           log.warn("此帳號尚未註冊: {}", userLoginRequest.getEmail());
-           throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        //檢查user是否存在
+        if (user == null) {
+            log.warn("此帳號尚未註冊: {}", userLoginRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
-        if(userLoginRequest.getPassword().equals(user.getPassword())){
+        //使用MD5 生成密碼雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+        //比較密碼
+        if (hashedPassword.equals(user.getPassword())) {
             return user;
         }
-        else{
+        else {
             log.warn("密碼錯誤，請再試一次");
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
